d71924f7d1ad21835caf70771528f08727b54165
Dylan Brain <dylanbrain@googlemail.com>
2021-03-26 10:21:58 +0900

    render expand added

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,4 +1,9 @@
+import { toDoitem } from "./createTodo";
+import { toDoListArray } from "./storage";
+
 let toDoListContainer = document.getElementById('toDoListContainer')
+let collapsableDivArr = []
+let contentDivArr = []
 
 // toggles form on and off
 let toggleForm = () => {
@@ -7,6 +12,7 @@ let toggleForm = () => {
 
 // render ToDo list items
 let renderToDoItem = (toDoListArray) => {
+
     console.log(toDoListArray)
     // for loop to render each object and create div
     for (let i = 0; i < toDoListArray.length; i ++) {
@@ -17,43 +23,66 @@ let renderToDoItem = (toDoListArray) => {
             div.className = 'toDoItem';
             toDoListContainer.appendChild(div);
 
-            // add variable to new div
-            let toDoItem = document.getElementById('toDoItem' + i)
+            // separate collapsableDiv and contentDiv
+
+            let collapsableDiv = document.createElement('div');
+            collapsableDiv.id = 'collapsableDiv'
+            collapsableDiv.className = 'collapsableDiv';
+            collapsableDivArr.push(collapsableDiv)
+            div.appendChild(collapsableDiv)
+
+            let contentDiv = document.createElement('div')
+            contentDiv.id = 'contentDiv';
+            contentDiv.className = 'contentDiv';
+            contentDivArr.push(contentDiv) 
+            div.appendChild(contentDiv)
 
             // dynamically create new divs for styling
-            let taskDiv = document.createElement('div');
-            taskDiv.id = 'taskDiv' + i;
-            taskDiv.className = 'taskDiv'
-            taskDiv.innerHTML = toDoListArray[i].task
-            toDoItem.appendChild(taskDiv)
-        
-            let descriptionDiv = document.createElement('div');
-            descriptionDiv.id = 'descriptionDiv' + i;
-            descriptionDiv.className = 'descriptionDiv';
-            descriptionDiv.innerHTML = toDoListArray[i].description;
-            toDoItem.appendChild(descriptionDiv)
-        
             let dueDateDiv = document.createElement('div');
             dueDateDiv.id = 'dueDateDiv' + i;
             dueDateDiv.className = 'dueDateDiv';
             dueDateDiv.innerHTML = toDoListArray[i].dueDate;
-            toDoItem.appendChild(dueDateDiv)
+            collapsableDiv.appendChild(dueDateDiv)
+
+            let taskDiv = document.createElement('div');
+            taskDiv.id = 'taskDiv' + i;
+            taskDiv.className = 'taskDiv'
+            taskDiv.innerHTML = toDoListArray[i].task
+            collapsableDiv.appendChild(taskDiv)
+
+            // divs for collapsed content
+            let descriptionDiv = document.createElement('div');
+            descriptionDiv.id = 'descriptionDiv' + i;
+            descriptionDiv.className = 'descriptionDiv';
+            descriptionDiv.innerHTML = toDoListArray[i].description;
+            contentDiv.appendChild(descriptionDiv)
         
             let notesDiv = document.createElement('div');
             notesDiv.id = 'notesDiv' + i;
             notesDiv.className = 'notesDiv';
             notesDiv.innerHTML = toDoListArray[i].notes;
-            toDoItem.appendChild(notesDiv)
+            contentDiv.appendChild(notesDiv)
         
             let priorityDiv = document.createElement('div');
             priorityDiv.id = 'priorityDiv' + i;
             priorityDiv.className = 'priorityDiv'
             priorityDiv.innerHTML = toDoListArray[i].priority;
-            toDoItem.appendChild(priorityDiv) 
+            contentDiv.appendChild(priorityDiv) 
         }
     }
+
+    // add ability to expand each item
+    collapsableDivArr.forEach(item => {
+        item.addEventListener('click', () => {
+            console.log(item.nextElementSibling)
+            let expandableContent = item.nextElementSibling
+            collapseRender(expandableContent);
+        })
+    })
+
     console.log(toDoListContainer)
     console.log(toDoListArray)
+
 }
 
 // remove currents divs to be reset depending on tab
@@ -64,8 +93,23 @@ let clearNodes = () => {
     }
 }
 
+// checks against CSS and opens and closes
+let collapseRender = (expandableContent) => {
+    console.log('collapse render called')
+    console.log(expandableContent)
+
+    if (expandableContent.style.display === 'block') {
+        expandableContent.style.display = 'none'
+    } else {
+        expandableContent.style.display = 'block'
+    }
+}
+
 export {
     toggleForm,
     renderToDoItem,
     clearNodes,
+    collapseRender,
+    collapsableDivArr,
+    contentDivArr
 }
